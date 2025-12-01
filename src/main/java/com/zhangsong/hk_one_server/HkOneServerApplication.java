58cf5bd7ea38a0071c9ff9e37f94f4506e96f1a6
G2 <2393579405@qq.com>
2025-12-01 23:39:27 +0800

    Initial commit

---
+++ src/main/java/com/zhangsong/hk_one_server/HkOneServerApplication.java
@@ -0,0 +1,13 @@
+package com.zhangsong.hk_one_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HkOneServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HkOneServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zhangsong/hk_one_server/HkOneServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhangsong.hk_one_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HkOneServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
